9d03ebfc80ab688e962d3db5223981003058b330
unknown <parimal.satyal@parnuprt01-500.nurun.com>
2015-08-18 11:40:14 +0200

    almost complete

--- mobility.rb
+++ mobility.rb
@@ -4,6 +4,10 @@ require 'rest-client'
 require 'net/http'
 require 'uri'
 
+# For CSS parsing
+require 'css_parser'
+include CssParser
+
 # This script requires Ruby and the gems Nokogiri and rest-client
 # Let's get started
 
@@ -23,25 +27,33 @@ user_agents = Hash[
 
 # First check with a Desktop UA and spit out all linked stylesheets
 page = Nokogiri::HTML(RestClient.get page_url, :user_agent => user_agents["WinDesktop"])
-desktop_html = page.css("link[rel=stylesheet]")
+desktop_stylesheets = page.css("link[rel=stylesheet]")
+desktop_alternate = page.css("link[rel=alternate]")
 
-puts "On Desktop: \n"
-puts desktop_html
+puts "On Desktop, using #{desktop_stylesheets.class}: \n"
+puts desktop_stylesheets
+puts "\n"
+# puts desktop_alternate
 
 # Then check with a mobile UA and spit out all linked stylesheets
 page = Nokogiri::HTML(RestClient.get page_url, :user_agent => user_agents["iPhone"])
-mobile_html = page.css("link[rel=stylesheet]")
+mobile_stylesheets = page.css("link[rel=stylesheet]")
+mobile_canonical = page.css("link[rel=canonical]")
 
 puts "On Phone: \n"
-puts mobile_html
+puts mobile_stylesheets
+puts "\n"
+# puts mobile_canonical
 
 # Then check if there's a difference in output between the two
 
-if desktop_html.to_s == mobile_html.to_s
+if desktop_stylesheets.to_s == mobile_stylesheets.to_s
 	puts "\nNot Dynamic serving"
 else
 	puts "\nDynamic serving"
+  dynamicserving = true
 end
+
 
 # Now to check if a mobile site exists with an m. prefix
 # First, get just the base URL and prefix the m. bit
@@ -53,8 +65,8 @@ if base.include?("www.")
 else
 	mobilesite = base.gsub("http://", "http://m.")
 end
- 
-puts "\nChecking for existence of mobile site at #{mobilesite}...\n"
+
+puts "\nChecking for existence of dedicated mobile site at #{mobilesite}...\n"
 
 # Now load that URL and see what response you get
 begin
@@ -62,12 +74,58 @@ begin
   case response.code
     when 200
      puts "Mobile site found with HTTP code #{response.code}. \n"
+    when 301
+     puts "Redirection to mobile site probable, HTTP code #{response.code} returned. \n"
     else
       response.return!(request, result, &block)
     end
   }
 rescue SocketError => e
-  puts "Mobile site does not exist at this address."
+  puts "Mobile site not found at this address."
 end
 
-# Next: Visit all CSS files (or limit to non-font files) and check for media queries. Get all breakpoints. 
+
+# Inspired by a PHP implementation (need to find author name)
+def parseMediaBlocks (rawcss)
+
+    mediablocks = []
+    start = 0
+
+    until (!(rawcss.index("@media") == start))
+      s = []
+        i = rawcss.index("{").begin(start)
+
+        if (!i)
+          s.push(rawcss[i])
+          i += 1
+
+          loop do
+            if (rawcss[i] == "{")
+              s.push("{")
+            elsif (rawcss[i] == "}")
+              s.delete("}")
+            end
+            i += 1
+            break if s.empty?
+          end
+
+          mediablocks = rawcss[start, ((i + 1) - start)]
+          start = i
+        end
+    end
+    return mediablocks
+end
+
+
+# Next: Visit all CSS files (or limit to non-font files) and check for media queries. Get all breakpoints.
+parser = CssParser::Parser.new
+
+desktop_stylesheets.each do |link|
+  url_css = link["href"].strip
+  parser.load_uri!(url_css)
+  raw_css = parser.to_s
+  puts "\nMedia query lines for #{url_css} are: \n
+  \n"
+  puts parseMediaBlocks(raw_css)
+
+end
